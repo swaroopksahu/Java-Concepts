b626a975f05d973277f07d466a09a3d255fe614e
Swaroop Sahu <swaroop.sahu@nokia.com>
2020-11-10 16:24:41 +0530

    Removed unnacessary imports.

--- src/StreamFlatMap.java
+++ src/StreamFlatMap.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class StreamFlatMap{
